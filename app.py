e84c48d88d7f1442912236cb5196b378b22becbe
Rekha R <r_rekha02@yahoo.com>
2024-08-05 23:40:34 +0000

    added more content

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ st.subheader("Hi, I'm Rekha working with my Streamlit experts here.:wave:")
 
 
 st.title ("I'm working on becoming a Streamlit Evangelist.")
+st.write("We're a passionate team wanted to learn and advance in Python to be more effective and efficient.")
+st.write("[Learn more >] (https://streamlit.io)")
