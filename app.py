038d5c490659540c4bd3d19563926d664af41b5f
Rekha R <r_rekha02@yahoo.com>
2024-08-05 23:20:44 +0000

    my first streamlit checkin

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I'm Rekha working with my Streamlit experts here.:wave:")
+
+
+st.title ("I'm working on becoming a Streamlit Evangelist.")
