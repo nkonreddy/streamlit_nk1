d6f1865ce02de8a92a28365d4c20f9b073f3bee9
Rekha R <r_rekha02@yahoo.com>
2024-08-06 00:21:00 +0000

    added what I do

--- app.py
+++ app.py
@@ -3,26 +3,33 @@ import streamlit as st
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-st.subheader("Hi, I'm Rekha working with my Streamlit experts here.:wave:")
+with st.container():
+    st.subheader("Hi, I'm Rekha working with my Streamlit experts here.:wave:")
 
 
 #st.title ("I'm working on becoming a Streamlit Evangelist.")
 
 
-st.markdown("""
-<style>
-.normal-font {
-    font-size:20px !important;
-}
-.title-font {
-    font-size:30px !important;
-}
+    st.markdown("""
+    <style>
+    .normal-font {
+        font-size:20px !important;
+    }
+    .title-font {
+        font-size:30px !important;
+    }
 
-</style>
-""", unsafe_allow_html=True)
-st.markdown('<p class="title-font">I\'m working on becoming a Streamlit Evangelist.', unsafe_allow_html=True)
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown('<p class="title-font">I\'m working on becoming a Streamlit Evangelist.', unsafe_allow_html=True)
 
 
-st.markdown('<p class="normal-font">We\'re a passionate team wanted to learn and advance in Python to be more effective and efficient.<br> <a href="https://streamlit.io/"> Learn more about Streamlit.', unsafe_allow_html=True)
+    st.markdown('<p class="normal-font">We\'re a passionate team wanted to learn and advance in Python to be more effective and efficient.<br> <a href="https://streamlit.io/"> Learn more about Streamlit.', unsafe_allow_html=True)
 #st.write("We're a passionate team wanted to learn and advance in Python to be more effective and efficient.")
 #st.write("[Learn more >] (https://streamlit.io)")
+with st.container():
+    st.write("---")
+    left_column, right_column = st.column(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
