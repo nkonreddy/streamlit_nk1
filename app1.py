f5684fed3a285526b6fda276a8d00da4e0dbe91e
nkonreddy <nkonreddy@yahoo.com>
2024-08-26 21:05:07 +0000

    change icon

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Webpage", page_icon=":globe_showing_asia_austr
 
 
 # .... Header Section .....
-st.subheader(" Hi, I am Naveen :folded_hands:")
+st.subheader(" Hi, I am Naveen :palm_tree:")
 st.title("A Design Consultant from Los Angeles")
-st.write( "I am passionate about finding ways to use Python and VBA to be more efficient")
+st.write( "I am passionate about finding ways to use Python and VBA to be more efficient and effective in my business settings")
 st.write("[Learn More>](https://pythonandvba.com)")
