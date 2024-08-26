cf1f4c5ba26f3f160fec9320ce725810cad6275b
nkonreddy <nkonreddy@yahoo.com>
2024-08-26 20:04:01 +0000

    intial entry

---
+++ app1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":globe_showing_asia_australia:", layout=wide)
+
+# .... Header Section .....
+st.subheader(" Hi, I am Naveen :folded_hands:")
+st.title("A Design Consultant from Los Angeles")
+st.write( "I am passionate about finding ways to use Python and VBA to be more efficient")
+st.write("[Learn More>](https://pythonandvba.com)")
