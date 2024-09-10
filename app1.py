3e4c5a2cb790694da012b2a16790d1e07d1d11a9
nkonreddy <nkonreddy@yahoo.com>
2024-09-10 00:12:28 +0000

    add comments

--- app1.py
+++ app1.py
@@ -21,21 +21,21 @@ def local_css(file_name):
 
 local_css("styles/style.css")
 
-
+#Lottie Files: https://lottiefiles.com/   !!Get animation files and its code!!
 #...Load Assets ...
 #lottie_coding = load_lottieurl("https://lottie.host/edcad0c9-6e21-4e9c-a1e8-82a2efaceefe/BXGDfV5g1u.json")
 #lottie_coding = ("https://lottie.host/2d55f4d0-ba83-4d59-9340-bc4fdb555162/u77Y0P0gjh.lottie")
-#lottie_coding = ("https://lottie.host/edcad0c9-6e21-4e9c-a1e8-82a2efaceefe/BXGDfV5g1u.json")
+lottie_coding = ("https://lottie.host/edcad0c9-6e21-4e9c-a1e8-82a2efaceefe/BXGDfV5g1u.json")
 #lottie_coding = "https://lottie.host/71d41eef-5a9d-41b4-b232-4960abb94a86/Uj3l65BbJf.json"
 #lottie_coding = "https://lottie.host/3a02227c-ba58-4ce6-a87a-6a81acce8828/9k1zbdwoE9.json"
-lottie_coding = "https://lottie.host/5024dba4-7826-4d99-a5dd-237eba5f0e5c/TqDv2koBcG.json"
+#lottie_coding = "https://lottie.host/5024dba4-7826-4d99-a5dd-237eba5f0e5c/TqDv2koBcG.json"
 img_contact_form = Image.open("images/yt_contact_form.png")
 img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
 # .... Header Section .....
 with st.container():
-    #st.subheader("Hi!, I am Naveen :palm_tree:")
-    st.subheader("Hi!, I am Naveen :folded_hands:")
+    st.subheader("Hi!, I am Naveen :palm_tree:")
+    #st.subheader("Hi!, I am Naveen :folded_hands:")
     st.title("A Design Consultant from Los Angeles")
     st.write( "I am passionate about finding ways to use Python and VBA to be more efficient and effective in my business settings")
     st.write("[Learn More>](https://pythonandvba.com)")
